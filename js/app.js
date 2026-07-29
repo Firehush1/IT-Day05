a63cea750267c8acb479975ad7fb1e1345b657e4
Firehush1 <hansanikalhari99@gmail.com>
2026-07-29 10:25:17 +0530

    style: comment out unused if statement and maintain if-else structure in app.js

--- js/app.js
+++ js/app.js
@@ -69,8 +69,21 @@
 
 //&& | || |!
 
-if(!false){
-    console.log("true");
+// if(!false){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
+//if else statement
+
+let x = 10;
+let y = 5;
+
+if(x > y){
+    console.log("x is greater than y");
+}else if(x < y){
+    console.log("x is less than y");
 }else{
-    console.log("false");
+    console.log("x is equal to y");
 }
